ff855a73aca01e5f6fb4dc4019d08688de171bd0
Siarhei Zaretski <sphincs.by@gmail.com>
2022-04-24 22:00:30 +0400

    Initial commit

---
+++ src/main/java/com/sphincs/cryptohelper/CryptohelperApplication.java
@@ -0,0 +1,13 @@
+package com.sphincs.cryptohelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptohelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptohelperApplication.class, args);
+	}
+
+}
